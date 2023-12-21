42450aa0d7728accbd9895ece814680e0492aeea
asrose274 <asrose274@gmail.com>
2023-12-21 10:06:41 -0700

    added some additional functions to the bookbot

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    words = word_count(text)
+    characters = char_count(text)
+    print(characters)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def word_count(text):
+    words = text.split()
+    count = len(words)
+    return count
+
+def char_count(text):
+    num_of_chars = {}
+    lower_text = text.lower()
+    for char in lower_text:
+        if(char in num_of_chars):
+            count = num_of_chars[char]
+            num_of_chars[char] = count + 1
+        else:
+            num_of_chars[char] = 1
+    return num_of_chars
 
 main()
